f8c578d34a58cac4f9198c16f64c6d644d094356
Mohammmad Kreishan <146862386+MohammedKreishan@users.noreply.github.com>
2024-07-03 21:51:58 +0300

    Portfoili Building

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,29 @@
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import './Skills.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import "./Skills.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Carousel from "react-bootstrap/Carousel";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
+import FormControl from "react-bootstrap/FormControl";
+import Accordion from "react-bootstrap/Accordion";
+import ExampleCarouselImage from './ExampleCarouselImage.png';
+
+
 
 function Skills() {
   return (
-    <div className='Skills'>
+    <div className="Skills">
+      
       <Container className="px-4 py-5" id="featured-3">
-        <h2 className="pb-2 border-bottom" style={{textAlign:"left"}}>Discover Our Featured Courses</h2>
+        <h2 className="pb-2 border-bottom" style={{ textAlign: "left" }}>
+          Discover Our Featured Courses
+        </h2>
         <Row className="g-4 py-5 row-cols-1 row-cols-lg-3">
           <Col>
             <Card className="feature d-flex flex-column h-100">
@@ -126,8 +139,8 @@ function Skills() {
         </Row>
       </Container>
 
-      <Container className="px-4 py-5" style={{textAlign:"left"}}>
-      <h2 className="pb-2 border-bottom">Unleash Your Learning Experience</h2>
+      <Container className="px-4 py-5" style={{ textAlign: "left" }}>
+        <h2 className="pb-2 border-bottom">Unleash Your Learning Experience</h2>
 
         <Row className="row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5">
           <Col className="d-flex flex-column align-items-start gap-2">
@@ -235,66 +248,77 @@ function Skills() {
         </Row>
       </Container>
 
-      <Container>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-        <Card style={{ width: '18rem', margin:"50px", className:"Skills-Card-1" }}>
-        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-        </ListGroup>
-        <Card.Body>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
-      </Card>
-          <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-          </ListGroup>
-          <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
-        </Container>
-        </div>
+      <Container className="px-4 py-5" id="featured-3">
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      </Container>
+
+      <Container className="px-4 py-5" id="featured-3">
+      <Carousel>
+      <Carousel.Item>
+            <img
+              src="https://res.cloudinary.com/dalwd5c23/image/upload/v1705177932/samples/cup-on-a-table.jpg"
+              alt="First slide"
+              style={{
+                width: "100%",
+                height: "500px",
+                objectFit: "cover",
+                
+              }}
+            />
+
+            <Carousel.Caption style={{color:"black"}}>
+              <h3>Welcome to Our E-Learning Platform</h3>
+              <p>Explore a world of knowledge and opportunities.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              src="https://res.cloudinary.com/dalwd5c23/image/upload/v1705177932/samples/cup-on-a-table.jpg"
+              alt="First slide"
+              style={{
+                width: "100%",
+                height: "500px",
+                objectFit: "cover",
+                
+              }}
+            />
+
+            <Carousel.Caption style={{color:"black"}}>
+              <h3>Welcome to Our E-Learning Platform</h3>
+              <p>Explore a world of knowledge and opportunities.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+
+    </Carousel>
+      </Container>
+    </div>
   );
 }
 
